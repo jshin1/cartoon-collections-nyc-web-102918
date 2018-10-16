459679b160a22836dd0a4ab73cd289dbd5a5260e
Ji S <j2yshin@gmail.com>
2018-10-16 02:00:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,11 @@ def roll_call_dwarves(array) # code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  array.collect do |element|
+    element.upcase + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
