f881588373f7c0f992ba9b6ed77bcd9251693423
Ji S <j2yshin@gmail.com>
2018-10-16 01:57:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,8 @@
 def roll_call_dwarves(array) # code an argument here
   # Your code here
   counter = 1
-  array.each do |name|
-    puts "#{counter}. #{name}"
-    counter += 1
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
   end
 end
 
