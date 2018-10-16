1b2d258fdd94e7bb095b70b1caeccd2280c67b24
Ji S <j2yshin@gmail.com>
2018-10-16 01:55:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+  counter = 1
+  array.each do |name|
+    puts "#{counter}. #{name}"
+    counter += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
