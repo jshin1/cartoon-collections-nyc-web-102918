da86821294c3e433d40cb581c5a1cbb9aec6e49c
Ji S <j2yshin@gmail.com>
2018-10-16 02:03:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,11 @@ def summon_captain_planet(array) # code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
+  array.any? do |call|
+    call.size > 4
+  end
 end
 
 def find_the_cheese# code an argument here
